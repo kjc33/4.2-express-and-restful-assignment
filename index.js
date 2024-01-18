e8b94b499da9b84746254b49526cfeadb81c0b38
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2024-01-18 15:58:30 -0800

    Routing Practice

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello, Koda!");
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("What up, Katie?");
+});
+
+app.post("/login", (req, res) => {
+  res.send("Post request at /login!");
+});
+
+app.put("/cart", (req, res) => {
+  res.send("Put request at /cart");
+});
+
+app.delete("/cart", (req, res) => {
+  res.send("Delete request at /cart");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
